d0ff7aff881d8de833fc7030bf358bf659298142
mhrndiva <ariadiva00@gmail.com>
2024-05-10 01:44:06 +0700

    bismillah

--- js/controller/get.js
+++ js/controller/get.js
@@ -1,6 +1,6 @@
 import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
-import { isiTabel } from "../temp/table.js";
+import { isiTabel } from "../temp/table.js"
 export function getallmahasiswa(results) {
     results.forEach(isiRow);
 }
