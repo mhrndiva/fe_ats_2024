aae81938dfcec7588ccf4038ffcc606286f66c25
mhrndiva <ariadiva00@gmail.com>
2024-07-02 16:57:47 +0700

    push

--- js/controller/post.js
+++ js/controller/post.js
@@ -4,23 +4,16 @@ import { urlPOST, AmbilResponse} from "../config/url_post.js";
 
 
 function pushData(){
-    var presensi = getValue("presensi");
 
     let data = {
         npm : getValue("npm"),
         matkul : {
             namamatkul : getValue("namamatkul"),
-            jadwal : getValue("jadwal"),
-            sks : getValue("sks"),
             dosen : getValue("dosen")
         },
         biodata : {
             nama : getValue("nama"),
-            npm : getValue("npm"),
-            phone_number : getValue("phone_number"),
             jurusan : getValue("jurusan"),
-            alamat : getValue("alamat"),
-            email : getValue("email"),
         },
         checkin : getValue("checkin"),
     }
