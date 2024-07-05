c3e7719c08bf2a817dc4e66829827df7ef53bb94
mhrndiva <ariadiva00@gmail.com>
2024-07-05 13:53:25 +0700

    push

---
+++ js/controller/put.js
@@ -0,0 +1,24 @@
+import { putData } from "https://bukulapak.github.io/api/process.js";
+import { onClick, getValue } from "https://bukulapak.github.io/element/process.js";
+import { urlPUT, AmbilResponse} from "../config/url_put.js";
+
+
+function pushData(){
+
+    let data = {
+        matkul : {
+            namamatkul : getValue("namamatkul"),
+            dosen : getValue("dosen")
+        },
+        biodata : {
+            nama : getValue("nama"),
+            npm: parseInt(getValue("npm"), 10),
+            jurusan : getValue("jurusan")
+        },
+        checkin : getValue("checkin"),
+    }
+    postData(urlPOST, data, AmbilResponse);
+
+}
+
+onClick("button", pushData);
