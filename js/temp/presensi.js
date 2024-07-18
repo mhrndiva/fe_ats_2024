fa2d17fa4c8ac0c970424a7a70059ad5b8474ab8
deviwlndrr <wdevi443@gmail.com>
2024-07-18 20:39:26 +0700

    up

--- js/temp/presensi.js
+++ js/temp/presensi.js
@@ -1,15 +1,6 @@
 export let isiTabel = `
 <tr class="g-gray-800">
-  <th class="whitespace-nowrap px-4 bg-white text-left">
-    <div class="flex items-center -m-2">
-      <div class="w-auto p-2">
-        <div class="flex items-center justify-center w-10 h-10 text-base font-medium text-#WARNALOGO#-600 bg-#WARNALOGO#-200 rounded-md">ULBI</div>
-      </div>
-      <div class="w-auto p-2">
-        <p class="text-xs font-semibold text-coolGray-800">#ID#</p>
-      </div>
-    </div>
-  </th>
+  
   <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-left">#Nama#</th>
   <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-left">#NPM#</th>
   <th class="whitespace-nowrap px-4 bg-white text-sm font-medium text-coolGray-800 text-center">#Jurusan#</th>
@@ -29,4 +20,4 @@ export let isiTabel = `
     </svg>
   </th>
 </tr>
-`;
+`
